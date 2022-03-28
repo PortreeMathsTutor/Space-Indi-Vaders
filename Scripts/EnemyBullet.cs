bfdbe8f381f5299fa468f6c18fccb35171cdd916
Sarah Ann Starmer <sarahannstarmer@yahoo.co.uk>
2022-03-28 16:33:06 +0100

    Update and rename EnemiyBullet.cs to EnemyBullet.cs

--- Scripts/EnemyBullet.cs
+++ Scripts/EnemyBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class EnemiyBullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
     public AudioClip death;
     public AudioSource _audioSource;
